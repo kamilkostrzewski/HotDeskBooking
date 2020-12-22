09920a6c2213d50975a25036dfc8a3f993db8bf6
Adrian <adrian.niedbala95@gmail.com>
2020-12-22 12:38:36 +0100

    Add login page to project.

--- HotDeskBooking.Web/ClientApp/src/app/app.module.ts
+++ HotDeskBooking.Web/ClientApp/src/app/app.module.ts
@@ -1,16 +1,27 @@
+import { MaterialModule } from './material.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HomeComponent } from './home/home.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ HotDeskBooking.Web/ClientApp/src/app/home/home.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  form = new FormGroup({
+    login: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required)
+  });
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  getError(errorType: string): string {
+    switch (errorType) {
+      case 'login':
+        return 'Login is required';
+
+      case 'password':
+        return 'Password is required';
+
+      default:
+        return 'ErrorType is not recognized';
+    }
+  }
+
+}
