8a92ef1238343c7977a2e92c874a6793481f3a50
Kamil Kostrzewski <kamil.kostrzewski95@gmail.com>
2020-12-23 16:47:16 +0100

    Add First Migration

---
+++ HotDeskBooking.Domain/Abstract/Entity.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace HotDeskBooking.Domain.Abstract
+{
+    public abstract class Entity
+    {
+        public Guid Id { get; protected set; }
+    }
+}
---
+++ HotDeskBooking.Persistence/Migrations/20201222183722_AddUsersTable.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HotDeskBooking.Persistence.Migrations
+{
+    public partial class AddUsersTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Login = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PasswordHash = table.Column<byte[]>(type: "varbinary(max)", nullable: true),
+                    PasswordSalt = table.Column<byte[]>(type: "varbinary(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
